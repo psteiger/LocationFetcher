b950789dda326db6e07bdeef9fb076860d14db1f
Patrick Steiger <psteiger@gmail.com>
2020-12-09 17:04:40 -0300

    Add experimental LocationSource.
    LocationSource allows for the user to either use a custom-set location
    or a real location obtained from LocationFetcher as the location source.
    
    Documentation to be done.

---
+++ locationfetcher/src/main/java/com/freelapp/libs/locationfetcher/impl/LocationSourceImpl.kt
@@ -0,0 +1,52 @@
+package com.freelapp.libs.locationfetcher.impl
+
+import android.location.Location
+import androidx.lifecycle.ProcessLifecycleOwner
+import androidx.lifecycle.lifecycleScope
+import com.freelapp.libs.locationfetcher.LocationFetcher
+import com.freelapp.libs.locationfetcher.LocationSource
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.*
+
+internal class LocationSourceImpl(
+    locationFetcher: LocationFetcher
+) : LocationSource {
+
+    private val locationSource = MutableStateFlow(LocationSource.Source.REAL)
+    private val _customLocation = MutableStateFlow<Location?>(null)
+    private val customLocation: SharedFlow<Location?> = _customLocation
+        .shareIn(
+            ProcessLifecycleOwner.get().lifecycleScope,
+            SharingStarted.WhileSubscribed(),
+            1
+        )
+
+    override val realLocation: SharedFlow<Location?> = locationFetcher.location
+        .shareIn(
+            ProcessLifecycleOwner.get().lifecycleScope,
+            SharingStarted.WhileSubscribed(),
+            1
+        )
+
+    @ExperimentalCoroutinesApi
+    override val location: SharedFlow<Location?> = locationSource
+        .flatMapLatest {
+            when (it) {
+                LocationSource.Source.REAL -> realLocation
+                LocationSource.Source.CUSTOM -> customLocation
+            }
+        }
+        .shareIn(
+            ProcessLifecycleOwner.get().lifecycleScope,
+            SharingStarted.WhileSubscribed(),
+            1
+        )
+
+    override fun setCustomLocation(location: Location) {
+        _customLocation.value = location
+    }
+
+    override fun setPreferredSource(source: LocationSource.Source) {
+        locationSource.value = source
+    }
+}
